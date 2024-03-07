f77f767cd167f64722025595c4e40dc013f40de6
Khizer Javed Iqbal <khizerjaved25@gmail.com>
2024-03-08 00:17:10 +0500

    done

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,4 +3,6 @@ export const LOGGED_IN_USER = "loggedInUser";
 
 export const TOKEN_TYPE = "Bearer ";
 export const REQUEST_HEADER_AUTH_KEY = "Authorization";
-export const SERVER_API = `${process.env.REACT_APP_SERVER_API}/api`;
+export const SERVER_API = process.env.REACT_APP_SERVER_API
+  ? `${process.env.REACT_APP_SERVER_API}/api`
+  : "/api";
